87ca792d1fbd41bf06b55eed0d4d46b89021c8bc
Hector Contreras <hikdul.lio@gmail.com>
2022-06-27 16:48:50 -0500

    control props no complete but almost valid

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,20 +1,12 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { onChangeArgs, product } from "../intenfaces/Interfaces"
 
 export const useProduc = ({ onChange, product, value = 0 }: useProducArgs) => 
 {
     const [counter, setCounter] = useState<number>(value)
-    //doble negacion para asegurar que me entregan numeros booleanos
-    // esto valor es para verificar si la funcion esta siendo controlado
-    const isControlled = useRef(!!onChange)
 
     const AddBy = (value: number ) => 
     {   
-            if(isControlled.current)
-            {
-                return onChange!({count: value, product})
-            }
-
             const newValue = Math.max(counter + value , 0)
             setCounter( newValue)
             onChange && onChange({count: newValue, product })
--- src/02-component-patterns/hooks/useShoppingCar.ts
+++ src/02-component-patterns/hooks/useShoppingCar.ts
@@ -1,28 +1,21 @@
 import { useState } from "react";
 import { onChangeEventArgs, ProductInCard } from "../intenfaces/Interfaces";
 
-
 export const useShoppingCar = () =>
 {
 
   const [shoppingCar, setShoppingCar] = useState<{[key: string]: ProductInCard }>({});
-
   const onProductCountChange = ({ count, product }: onChangeEventArgs) => {
-    setShoppingCar((oldShoppingCar) => {
-      const prodInCart: ProductInCard = oldShoppingCar[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(prodInCart.count + count, 0) > 0) {
-        prodInCart.count += count;
-        return {
-          ...oldShoppingCar,
-          [product.id]: prodInCart,
-        };
-      }
 
-      const {[product.id]: toDelete, ...rest} = oldShoppingCar
-      return rest
+    setShoppingCar((oldShoppingCar) => {
+      if(count === 0){
+        const { [product.id]: toDelete, ...rest} = oldShoppingCar
+        return rest
+      }
+      return{
+        ...oldShoppingCar,
+        [product.id]: {...product, count}
+      }
     })
   }
 
