c20944a85ee416d505ba268995b150a8db3a4d7e
Hector Contreras <hikdul.lio@gmail.com>
2022-06-27 16:33:18 -0500

    pasando todo a un par de componentes para separar un poco el codigo

---
+++ src/02-component-patterns/components/RenderShoppingCard.tsx
@@ -0,0 +1,29 @@
+import { onChangeEventArgs, product, ProductInCard } from "../intenfaces/Interfaces"
+import { ProducButtons } from "./ProductButtons"
+import { ProductCard } from "./ProductCard"
+import { ProductImage } from "./ProductImage"
+
+
+export const RenderShoppingCard = ({onChange, product}: RenderShoppingCardProps) =>
+{
+    return(
+          <ProductCard
+            style={{
+              width: "100px",
+            }}
+            onChange={onChange}
+            product={product}
+            value={product.count}
+            className="bg-dark text-white"
+          >
+            <ProductImage className="custom-image" />
+            <ProducButtons className="custom-buttons" />
+          </ProductCard>
+    )
+}
+
+interface RenderShoppingCardProps
+{
+    onChange?: (props : onChangeEventArgs) => void 
+    product:  ProductInCard 
+}
---
+++ src/02-component-patterns/const/Productos.ts
@@ -0,0 +1,18 @@
+import { product } from "../intenfaces/Interfaces";
+
+export const PRODUCTOS: product[] = [
+  {
+    id: "1",
+    title: "Cards",
+  },
+  {
+    id: "2",
+    title: "Coffee Mug",
+    img: "/coffee-mug.png",
+  },
+  {
+    id: "3",
+    title: "Coffee Mug - Meme",
+    img: "/coffee-mug2.png",
+  },
+];
