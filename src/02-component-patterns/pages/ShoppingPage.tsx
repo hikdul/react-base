9102bf441d92c4da7c8ec8e54e52d7b6699eaf8a
Hector Contreras <hikdul.lio@gmail.com>
2022-06-27 16:42:39 -0500

    control props implement

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -2,43 +2,19 @@ import { useState } from "react";
 import { RenderProducts } from "../components";
 import { RenderShoppingCard } from "../components/RenderShoppingCard";
 import { PRODUCTOS } from "../const/Productos";
+import { useShoppingCar } from "../hooks/useShoppingCar";
 import { onChangeEventArgs, ProductInCard } from "../intenfaces/Interfaces";
 import "../styles/custom-styles.css";
 
 export const ShoppingPage = () => {
-  const [shoppingCar, setShoppingCar] = useState<{[key: string]: ProductInCard }>({});
-
-  const onProductCountChange = ({ count, product }: onChangeEventArgs) => {
-    setShoppingCar((oldShoppingCar) => {
-      const prodInCart: ProductInCard = oldShoppingCar[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(prodInCart.count + count, 0) > 0) {
-        prodInCart.count += count;
-        return {
-          ...oldShoppingCar,
-          [product.id]: prodInCart,
-        };
-      }
-
-      const {[product.id]: toDelete, ...rest} = oldShoppingCar
-      return rest
-    })
-  }
+  const { shoppingCar, onProductCountChange } = useShoppingCar()
 
   return (
     <div>
       <h1>Shoppin store</h1>
       <hr />
       {/* para ver los productos grandes */}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-        }}
+      <div style={{display: "flex",flexDirection: "row",flexWrap: "wrap",justifyContent: "space-between"}}
       >
         {PRODUCTOS.map((prod) => (
           <RenderProducts product={prod} value={shoppingCar[prod.id]?.count || 0} onChange={(event) => onProductCountChange(event)}/>
@@ -52,5 +28,5 @@ export const ShoppingPage = () => {
       </div>
       <hr />
     </div>
-  );
-};
+  )
+}
