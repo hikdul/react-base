5e358d4ab42dca56ed284600af2b9b70863a0ffe
Hector Contreras <hikdul.lio@gmail.com>
2022-07-01 12:02:07 -0500

    pre npm hikdul

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,7 +1,6 @@
 import { ProducButtons, ProductImage, ProductTitle } from "../components";
 import { ProductCard } from "../components/ProductCard";
 import { PRODUCTOS } from "../const/Productos";
-import "../styles/custom-styles.css";
 
 export const ShoppingPage = () => {
 
@@ -15,22 +14,13 @@ export const ShoppingPage = () => {
           <ProductCard 
            key={prod.id}  
            product={prod} 
-           className="bg-dark text-white"
-           style={{margin: '10px'}}
            initialValues={{count:0, maxCount: prod.cant}}>
              {
               ({reset, count, increaseBy, isMaxCountReached})=>(
                 <>
-                  <ProductImage 
-                    className="custom-image" 
-                    style={{boxShadow: '10px 10px 10px rgba(0,0,0,.2)'}} />
-                  <ProductTitle className="text-bold"/>
-                  <ProducButtons className="custom-buttons"/>
-                  <button onClick={reset} >Reset</button>
-                   { !isMaxCountReached && <button onClick={ ()=> increaseBy(2) } >+2</button> }
-                  { count > 0 && <button onClick={ ()=> increaseBy(-2) } >-2</button> }                 
-                  
-                  <span>{count}</span>
+                  <ProductImage />
+                  <ProductTitle />
+                  <ProducButtons />
                 </>
               
               )
