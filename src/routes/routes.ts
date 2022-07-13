b0f9385d3f63511feac038dae8eb3bc967a4f8b3
Hector Contreras <hikdul.lio@gmail.com>
2022-07-13 12:51:23 -0500

    nada

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,10 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { LazyPage1, LazyPage3 } from "../01-lazyload/pages";
+import { RegisterPage } from "../03-forms/pages/RegisterPage";
 import { RouteData } from "./types/types";
 
 
-
 export const routes: RouteData[] = [
-    {to: '/', path:'', Component: LazyPage2, name: 'Shop' },
+    {to: '/register', path:'register', Component: RegisterPage, name: 'Register' },
     {to: '/about', path:'About', Component: LazyPage1, name: 'About' },
     {to: '/contact', path:'Contact', Component: LazyPage3, name: 'Contact' },
 ]
